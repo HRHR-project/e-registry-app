e3d1aa94a07705a96961cd442d0be3005c1b9bcc
zepto5 <andre.hukkelaas@abaris.no>
2017-08-03 15:52:57 +0200

    ERegistry app

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,108 @@
 'use strict';
 
 var webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 var colors = require('colors');
 
-const isDevBuild = process.argv[1].indexOf('webpack-dev-server') !== -1;
-const dhisConfigPath = process.env.DHIS2_HOME && `${process.env.DHIS2_HOME}/config`;
+const dhisConfigPath = process.env.DHIS2_HOME && `${process.env.DHIS2_HOME}/config.json`;
 let dhisConfig;
 
 try {
+    console.log(dhisConfigPath);
     dhisConfig = require(dhisConfigPath);
     console.log('\nLoaded DHIS config:');
 } catch (e) {
     // Failed to load config file - use default config
-    console.warn(`\nWARNING! Failed to load DHIS config:`, e.message);
+    console.warn('\nWARNING! Failed to load DHIS config:', e.message);
     console.info('Using default config');
     dhisConfig = {
-        baseUrl: 'http://localhost:8080/dhis',
-        authorization: 'Basic YWRtaW46ZGlzdHJpY3Q=', // admin:district
+        baseUrl: 'http://localhost:9090/dhis',
+        authorization: 'Basic YWRtaW46ZGlzdHJpY3Q=' // admin:district
     };
 }
 console.log(JSON.stringify(dhisConfig, null, 2), '\n');
 
-function log(req, res, opt) {
+function bypass(req, res, opt) {
     req.headers.Authorization = dhisConfig.authorization;
-    console.log('[PROXY]'.cyan.bold, req.method.green.bold, req.url.magenta, '=>'.dim, opt.target.dim);
 }
 
-const webpackConfig = {
+function makeLinkTags(stylesheets) {
+    return function (hash) {
+        return stylesheets
+            .map(([url, attributes]) => {
+                const attributeMap = Object.assign({ media: 'screen' }, attributes);
+
+                const attributesString = Object
+                    .keys(attributeMap)
+                    .map(key => `${key}="${attributeMap[key]}"`)
+                    .join(' ');
+
+                return `<link type="text/css" rel="stylesheet" href="${url}?_=${hash}" ${attributesString} />`;
+            })
+            .join(`\n`);
+    };
+}
+
+function makeScriptTags(scripts) {
+    return function (hash) {
+        return scripts
+            .map(script => (`<script src="${script}?_=${hash}"></script>`))
+            .join(`\n`);
+    };
+}
+
+module.exports = {
     context: __dirname,
-    contentBase: __dirname,
-    entry: './src/app.js',
-    devtool: 'source-map',
+    entry: './scripts/index.js',
     output: {
-        path: __dirname + '/build',
-        filename: 'app.js',
-        publicPath: 'http://localhost:8081/',
+        path: path.join(__dirname, '/build'),
+        filename: 'app-[hash].js'
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'stage-0', 'react'],
-                },
+                exclude: [/(node_modules)/],
+                loaders: ['ng-annotate-loader', 'babel-loader'],
             },
             {
                 test: /\.css$/,
-                loader: 'style!css',
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style!css!sass',
+                loader: 'style-loader!css-loader',
             },
         ],
     },
-    resolve: {
-        alias: {
-            react: path.resolve('./node_modules/react'),
-            'material-ui': path.resolve('./node_modules/material-ui'),
-        },
-    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new HTMLWebpackPlugin({
+            template: './index.ejs',
+            stylesheets: makeLinkTags([
+                ['styles/style.css'],
+                ['styles/print.css', { media: 'print' }],
+            ]),
+            scripts: makeScriptTags([
+                'core/tracker-capture.js',
+                '../main.js',
+            ]),
+        }),
+    ],
+    devtool: ['sourcemap'],
     devServer: {
+        contentBase: '.',
         progress: true,
         colors: true,
         port: 8081,
-        inline: true,
-        compress: true,
+        inline: false,
+        compress: false,
         proxy: [
-            { path: '/api/*', target: dhisConfig.baseUrl, bypass: log },
-            { path: '/dhis-web-commons/*', target: dhisConfig.baseUrl, bypass: log },
-            { path: '/icons/*', target: dhisConfig.baseUrl, bypass: log },
-            { path: '/css/*', target: 'http://localhost:8081/build', bypass: log },
-            { path: '/jquery.min.js', target: 'http://localhost:8081/node_modules/jquery/dist', bypass: log },
-            { path: '/polyfill.min.js', target: 'http://localhost:8081/node_modules/babel-polyfill/dist', bypass: log },
+                { path: '/api/**', target: dhisConfig.baseUrl, bypass:bypass },
+                { path: '/dhis-web-commons-ajax-json/**', target: dhisConfig.baseUrl, bypass:bypass },
+                { path: '/dhis-web-commons-stream/**', target: dhisConfig.baseUrl, bypass:bypass },
+                { path: '/dhis-web-commons/**', target: dhisConfig.baseUrl, bypass:bypass, proxyTimeout: 1000 * 60 * 5 },
+                { path: '/dhis-web-core-resource/**', target: dhisConfig.baseUrl, bypass:bypass },
+                { path: '/icons/**', target: dhisConfig.baseUrl, bypass:bypass },
+                { path: '/images/**', target: dhisConfig.baseUrl, bypass:bypass },
+                { path: '/main.js', target: dhisConfig.baseUrl, bypass:bypass },
         ],
     },
 };
-
-if (!isDevBuild) {
-    webpackConfig.plugins = [
-        // Replace any occurance of process.env.NODE_ENV with the string 'production'
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"',
-            DHIS_CONFIG: JSON.stringify({}),
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            //     compress: {
-            //         warnings: false,
-            //     },
-            comments: false,
-            beautify: true,
-        }),
-    ];
-} else {
-    webpackConfig.plugins = [
-        new webpack.DefinePlugin({
-            DHIS_CONFIG: JSON.stringify(dhisConfig)
-        }),
-    ];
-}
-
-module.exports = webpackConfig;
