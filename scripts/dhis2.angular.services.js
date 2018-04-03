b709c6a3899ef4cf210385ef2dd873799f0e3ee3
Bekkalizer <markus.bekken@gmail.com>
2018-04-03 15:57:40 +0200

    Added support for AGE in program rule engine

--- scripts/dhis2.angular.services.js
+++ scripts/dhis2.angular.services.js
@@ -1123,7 +1123,7 @@ var d2Services = angular.module('d2Services');
             processedValue = $filter('trimquotes')(processedValue);
 
             //Append single quotation marks in case the variable is of text or date type:
-            if(valueType === 'LONG_TEXT' || valueType === 'TEXT' || valueType === 'DATE' || valueType === 'OPTION_SET' ||
+            if(valueType === 'LONG_TEXT' || valueType === 'TEXT' || valueType === 'DATE' || valueType === 'OPTION_SET' || valueType === 'AGE' ||
                 valueType === 'URL' || valueType === 'DATETIME' || valueType === 'TIME' || valueType === 'PHONE_NUMBER' || valueType === 'ORGANISATION_UNIT') {
                 if(processedValue) {
                     processedValue = "'" + processedValue + "'";
