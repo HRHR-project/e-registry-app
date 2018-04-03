f80ee717bcfef746f489c74569a1a4b8b08b0a25
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2018-04-03 16:56:30 +0200

    Fix for #63: hidd previous values table for some sections

--- scripts/dhis2.angular.services.js
+++ scripts/dhis2.angular.services.js
@@ -1124,7 +1124,7 @@ var d2Services = angular.module('d2Services');
 
             //Append single quotation marks in case the variable is of text or date type:
             if(valueType === 'LONG_TEXT' || valueType === 'TEXT' || valueType === 'DATE' || valueType === 'OPTION_SET' ||
-                valueType === 'URL' || valueType === 'DATETIME' || valueType === 'TIME' || valueType === 'PHONE_NUMBER' || valueType === 'ORGANISATION_UNIT') {
+                valueType === 'URL' || valueType === 'DATETIME' || valueType === 'TIME' || valueType === 'PHONE_NUMBER' || valueType === 'ORGANISATION_UNIT' || valueType === 'AGE') {
                 if(processedValue) {
                     processedValue = "'" + processedValue + "'";
                 } else {
